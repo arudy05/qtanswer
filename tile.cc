1e068b87a98f0fcdd7c66eb98d046bb030f02f00
arudy05 <arudy1305@gmail.com>
2025-05-27 15:26:11 -0600

    Add Tile class files (oops) also made it so that tiles now expand to fill the window

---
+++ tile.cc
@@ -0,0 +1,18 @@
+#include "tile.h"
+
+
+Tile::Tile(int val, int cat): value{val}, category{cat}, button{new QPushButton(QString::number(val))} {
+    connect(button, SIGNAL (clicked(bool)), this, SLOT (onButtonPress()));
+    button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+}
+
+Tile::~Tile() {
+    // you get the point
+    delete button;
+}
+
+void Tile::onButtonPress() {
+    // When the button is pressed, emit a signal with this tile's
+    // value and category so that the game pulls the proper question.
+    emit tilePressed(value, category);
+}
---
+++ tile.h
@@ -0,0 +1,26 @@
+#ifndef TILE_H
+#define TILE_H
+
+#include <QPushButton>
+
+// A Tile object is essentially just a wrapper for a QPushButton
+// with some extra stuff specific to this application
+class Tile : public QObject {
+    Q_OBJECT
+    int value;
+    int category;
+
+public:
+    QPushButton *button;
+    Tile(int val, int cat);
+    ~Tile();
+
+public slots:
+    void onButtonPress();
+
+signals:
+    void tilePressed(int val, int cat);
+
+};
+
+#endif // TILE_H
