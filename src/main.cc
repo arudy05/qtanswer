53a83aae5b5bde92d3aa4d67336c0d892838fff2
arudy05 <arudy1305@gmail.com>
2025-06-20 15:51:02 -0600

    Added version number and git hash in window title This was mostly taken from https://gist.github.com/Tordan/c4d4d14aac0e85ebf7122ed2189ea387, with some adaptations for this project's purposes.

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,13 @@
 #include <QApplication>
 
 #include "gamewindow.h"
+#include "version.h"
 
 int main(int argc, char **argv) {
     QApplication app (argc, argv);
 
     GameWindow window;
+    window.setWindowTitle(WINDOW_HEADER);
 
     window.show();
     return app.exec();
---
+++ version.cmake
@@ -0,0 +1,18 @@
+# Based off of https://gist.github.com/Tordan/c4d4d14aac0e85ebf7122ed2189ea387
+set (GIT_HASH "unknown")
+
+find_package(Git QUIET)
+if (GIT_FOUND)
+    execute_process(
+        COMMAND git log -1 --format=%h
+        OUTPUT_VARIABLE GIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_QUIET
+    )
+endif()
+
+configure_file(
+    ${IN_FILE}
+    ${OUT_FILE}
+    @ONLY
+)
